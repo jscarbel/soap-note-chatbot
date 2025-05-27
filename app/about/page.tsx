9ee65626d4f0c742450bf75cd3fb412e209a5ba6
Jaimie Scarbel <jaimie@Jaimies-MacBook-Pro.local>
2025-05-27 12:57:01 -0400

    updated custom colors on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,10 +6,10 @@ export default function AboutPage() {
       <Nav />
       <section className="bg-gray-100 py-20">
   <div className="container mx-auto px-4 text-center">
-    <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">
+    <h1 className="text-4xl md:text-5xl font-bold text-customFont-800 mb-4">
       About Us
     </h1>
-    <p className="text-lg md:text-xl text-gray-600 max-w-2xl mx-auto">
+    <p className="text-lg md:text-xl text-cuustomFont-600 max-w-2xl mx-auto">
       Learn more about our mission to make notes SOAP notes easier, and work stay at work
     </p>
   </div>
