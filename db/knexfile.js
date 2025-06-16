a2c7fd62425870b5c5467ded2a5442e5edb8c751
Jaimie Scarbel <jaimiescarbel@gmail.com>
2025-06-16 12:11:35 -0400

    initialized backend

---
+++ db/knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      filename: './'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'soape',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
