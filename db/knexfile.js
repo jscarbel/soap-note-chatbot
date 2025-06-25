c8cd0cd3e14d2c2938d22123240d236b308e0dd8
Jaimie Scarbel <jaimiescarbel@gmail.com>
2025-06-25 13:54:38 -0400

    updated knexfile.js

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,45 +3,43 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+require('dotenv').config();
 
-  development: {
-    client: 'postgresql',
-    connection: {
-      filename: './'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_NAME,
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'soape',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+export const development = {
+  client: 'pg',
+  connection: {
+    filename: './'
   }
-
+};
+export const staging = {
+  client: 'pg',
+  connection: {
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
+export const production = {
+  client: 'postgresql',
+  connection: {
+    database: 'soape',
+    user: 'username',
+    password: 'password'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 };
