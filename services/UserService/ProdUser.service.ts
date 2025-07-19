3e93cea8dc4c76ee0a47dda9e8fcf602c47fed66
rscarbel <ryanscarbel@gmail.com>
2025-07-19 13:48:43 -0400

    oops, forgot to save the file

--- services/UserService/ProdUser.service.ts
+++ services/UserService/ProdUser.service.ts
@@ -1,10 +1,11 @@
 import { Email } from '../../models/email.model';
 import { User } from '../../models/user.model';
+import { NotImplementedError } from '../../utils/errors';
 import { IUserService } from './IUser.service';
 
 export class ProdUserService implements IUserService {
   /** @inheritdoc */
   getUserByEmail(email: Email): Promise<User> {
-    throw new Error('Method not implemented.');
+    throw new NotImplementedError();
   }
 }
